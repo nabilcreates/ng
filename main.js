5208e8c25582a06fad4a77b9840be0a20f70b69c
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-12 17:34:43 +0800

    Added evaluate_number

--- main.js
+++ main.js
@@ -17,6 +17,24 @@ class NumberGenerator {
         return number;
     }
 
+    evaluate_number(start, stop, req) {
+        // RUN THE LOOP 100 TIMES
+        for (var i = 0; i < 100; i++) {
+
+            // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
+            var num_eval = this.generateNumber({
+                length: 8
+            })
+
+            // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
+            if (num_eval.toString().slice(start, stop) == req) {
+
+                // RETURN THE NUMBER ITSELF
+                return num_eval
+            }
+        }
+        
+    }
 }
 
 /*
@@ -27,25 +45,4 @@ EXTERNAL JS FILE
 */
 
 // NEW INSTANCE OF CLASS
-var numbergenerator = new NumberGenerator()
-
-// RUN THE LOOP 100 TIMES
-for (var i = 0; i < 10000; i++) {
-
-    // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
-    var num_eval = numbergenerator.generateNumber({
-        length: 8
-    })
-
-    console.log(num_eval)
-    
-    // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
-    if (num_eval.toString().slice(0, 1) == 9) {
-
-        // PUSH IT TO THE ARRAY
-        allnumbers.push(num_eval)
-    }
-}
-
-// CONSOLE.LOG THE ARRAY
-console.log(allnumbers)
+// var numbergenerator = new NumberGenerator()
