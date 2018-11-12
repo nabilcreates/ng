345749f21ebb54f99a1923710eb241215dffb42a
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-12 17:22:37 +0800

    Fixed so that numbers generated (each digit is now max 9 instead of 8)

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class NumberGenerator {
         let number = '';
 
         for (var i = 0; i < options.length; i++) {
-            number += Math.floor(Math.random() * 9) + ''
+            number += Math.floor(Math.random() * 10) + ''
         }
 
         return number;
@@ -30,15 +30,17 @@ EXTERNAL JS FILE
 var numbergenerator = new NumberGenerator()
 
 // RUN THE LOOP 100 TIMES
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 10000; i++) {
 
     // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
     var num_eval = numbergenerator.generateNumber({
         length: 8
     })
+
+    console.log(num_eval)
     
     // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
-    if (num_eval.toString().slice(0, 1) == 8) {
+    if (num_eval.toString().slice(0, 1) == 9) {
 
         // PUSH IT TO THE ARRAY
         allnumbers.push(num_eval)
