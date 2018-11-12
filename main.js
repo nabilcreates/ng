657ee61c4b8a5dbba8c0e918a26ed9006431e7e9
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-12 20:07:47 +0800

    Refactoring and also adding .writeTo method (READ README.md)

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+var fs = require('fs')
+
 /**
  * =========================================
+ * =========================================
  * MAIN CODE
+ * MAIN CODE
+ * MAIN CODE
+ * MAIN CODE
+ * =========================================
  * =========================================
  */
 class NumberGenerator {
@@ -27,25 +34,52 @@ class NumberGenerator {
     }
 
     evaluate_number(start, stop, req) {
-        if (start && stop && req) {
-            // RUN THE LOOP 100 TIMES
-            for (var i = 0; i < 100; i++) {
+        // RUN THE LOOP 100 TIMES
+        for (var i = 0; i < 100; i++) {
 
-                // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
-                var num_eval = this.generateNumber({
-                    length: 8
-                })
+            // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
+            var num_eval = this.generateNumber({
+                length: 8
+            })
 
-                // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
-                if (num_eval.toString().slice(start, stop) == req) {
+            // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
+            if (num_eval.toString().slice(start, stop) == req) {
 
-                    // RETURN THE NUMBER ITSELF
-                    return num_eval
-                }
+                // RETURN THE NUMBER ITSELF
+                return num_eval
+
             }
-        }else{
-            throw new Error('Please enter a start,stop and the requirement in the arguments!')
         }
 
     }
+
+    writeTo(start, stop, req, times, writename) {
+        // CLEAR THE FILE (OVERWRITE WITH BLANK)
+        fs.writeFile(writename, ``, {
+            flag: 'w'
+        })
+
+        // LOOP 10000 TIMES
+        for (var i = 0; i < times; i++) {
+
+            // AN INSTANCE OF EVALUATE_NUMBER IS CREATED WITH SPECIFIC PATTERN
+            var num = this.evaluate_number(start, stop, req)
+
+            // CONSOLE LOG (DEBUG)
+            // console.log(i)
+            // console.log(num !== undefined)
+
+            // NUMBERS THAT DOESNT MEET THE PATTERN WILL RETURN UNDEFINED, IF IT IS NOT UNDEFINED THEN WRITE IT TO THE FILE
+            if (num !== undefined) {
+                fs.writeFile(writename, `${i}: ${num} \n`, {
+                    flag: 'a'
+                }, function () {
+                    console.log(`DONE`)
+                })
+            }
+
+        }
+    }
 }
+
+var ng = new NumberGenerator()
