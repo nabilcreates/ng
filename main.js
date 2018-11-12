7c170959b4f7905f83c1c2914121bdd6da852e61
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-12 17:16:58 +0800

    Moved code to class

--- main.js
+++ main.js
@@ -1,47 +1,33 @@
-var fs = require('fs')
+var allnumbers = []
 
-function generateNumber(length) {
+class NumberGenerator {
 
-    let number = '';
+    generateNumber(options) {
 
-    for (var i = 0; i < length; i++) {
-        number += Math.floor(Math.random() * 9) + ''
+        let number = '';
+
+        for (var i = 0; i < options.length; i++) {
+            number += Math.floor(Math.random() * 9) + ''
+        }
+
+        return number;
     }
 
-    return number;
 }
 
-function evaluateNumber() {
+var numbergenerator = new NumberGenerator()
 
-    let number_eval = generateNumber(8);
+for (var i = 0; i < 100; i++) {
+
+    var num_eval = numbergenerator.generateNumber({
+        length: 8
+    })
     
-    // IF FIRST DIGIT IS NOT EQUAL TO 8
-    // TO STRING CONVERTS IT TO STRING
-    // SLICE BASICALLY SLICES FROM INDEX 0 TO 1
-    if (number_eval.toString().slice(0, 1) !== '8') {
-
-        // RUN THE FUNCTION AGAIN
-        setTimeout(() => {
-            evaluateNumber()
-        }, 1)
-    } else {
-
-        // CONSOLE LOG THE NUMBER
-        return number_eval.toString()
+    // IS 8
+    if (num_eval.toString().slice(0, 1) == 8) {
+        allnumbers.push(num_eval)
     }
 }
 
-function writing(){
 
-    var data = evaluateNumber()
-    
-    if(data == undefined){
-        data = 'no match'
-    }
-    
-    fs.writeFileSync('./cool.txt' , data + '\n' , {flag: 'a'})
-}
-
-for (var i = 0; i < 500; i++) {
-    writing()
-}
+console.log(allnumbers)
