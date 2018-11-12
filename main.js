66558f9870e16c118c76b4d3945a683e48eda82a
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-12 17:21:01 +0800

    Commenting

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var allnumbers = []
-
+/**
+ * =========================================
+ * MAIN CODE
+ * =========================================
+ */
 class NumberGenerator {
 
     generateNumber(options) {
@@ -15,19 +19,31 @@ class NumberGenerator {
 
 }
 
+/*
+==============================================
+THE CODE BELOW IS AN EXAMPLE TO BE USED IN AN
+EXTERNAL JS FILE
+==============================================
+*/
+
+// NEW INSTANCE OF CLASS
 var numbergenerator = new NumberGenerator()
 
+// RUN THE LOOP 100 TIMES
 for (var i = 0; i < 100; i++) {
 
+    // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
     var num_eval = numbergenerator.generateNumber({
         length: 8
     })
     
-    // IS 8
+    // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
     if (num_eval.toString().slice(0, 1) == 8) {
+
+        // PUSH IT TO THE ARRAY
         allnumbers.push(num_eval)
     }
 }
 
-
+// CONSOLE.LOG THE ARRAY
 console.log(allnumbers)
