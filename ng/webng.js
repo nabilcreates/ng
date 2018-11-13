2717dbe970275cf5926f23c782d25a51cc49a571
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-13 16:35:30 +0800

    Refactored code (and update to latest version)

--- ng/webng.js
+++ ng/webng.js
@@ -1,4 +1,3 @@
-
 /**
  * =========================================
  * =========================================
@@ -6,52 +5,39 @@
  * MAIN CODE
  * MAIN CODE
  * MAIN CODE
- * 
- * WEBNG DOESNT SUPPORT .writeTo()
  * =========================================
  * =========================================
  */
 class NumberGenerator {
 
-    generateNumber(options) {
+    generateNumber(length = 8) {
 
-        if (options) {
-            // NUMBER IS EMPTY (SO IT CAN CONCAT LATER)
-            let number = '';
+        // NUMBER IS EMPTY (SO IT CAN CONCAT LATER)
+        let number = '';
 
-            // LOOPS THROUGH THE OPTION (LENGTH) THAT IS GIVEN AS AN ARGUMENT
-            for (var i = 0; i < options.length; i++) {
+        // LOOPS THROUGH THE OPTION (LENGTH) THAT IS GIVEN AS AN ARGUMENT
+        for (var i = 0; i < length; i++) {
 
-                // CONCAT WITH NUMBER
-                number += Math.floor(Math.random() * 10) + ''
-            }
-
-            // RETURN THE NUMBER
-            return number;
-        } else {
-            throw new Error('Please enter an option!')
+            // CONCAT WITH NUMBER
+            number += Math.floor(Math.random() * 10) + '';
         }
+
+        // RETURN THE NUMBER
+        return number;
 
     }
 
-    evaluate_number(start, stop, req) {
-        // RUN THE LOOP 100 TIMES
-        for (var i = 0; i < 100; i++) {
+    evaluate_number(num_eval, start, stop, req) {
+        // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
+        if (num_eval.toString().slice(start, stop) == req) {
 
-            // EACH TIME IT RUNS NUM_EVAL IS GOING TO RUN THE FUNCTION BELOW (WHICH WEILL GENERATE DIFFERENT NUMBERS EACH TIME)
-            var num_eval = this.generateNumber({
-                length: 8
-            })
+            // RETURN THE NUMBER ITSELF
+            return num_eval;
 
-            // CONVERT THE NUMER TO STRING AND SLICE FROM 0 TO 1 SO IT WILL RETURN THE FIRST DIGIT, AND IF ITS EQUAL TO 8
-            if (num_eval.toString().slice(start, stop) == req) {
-
-                // RETURN THE NUMBER ITSELF
-                return num_eval
-
-            }
         }
 
     }
 
 }
+
+var ng = new NumberGenerator();
